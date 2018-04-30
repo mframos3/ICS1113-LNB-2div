2eea243c25e7da5fc1d6d8db375d9305066d6d2c
Benja <bffarias@uc.cl>
2018-04-30 19:42:57 -0300

    Modelo Completo (revisar)

--- proyecto_optimizacion.py
+++ proyecto_optimizacion.py
@@ -65,11 +65,12 @@ modelo.setObjective(objetivo,GRB.MINIMIZE)
 # R1
 for local in equipos:
     for visita in equipos:
-        modelo.addConstr(quicksum(partido_ijt[local,visita,fecha] for fecha in fechas) == 1)
+        if local != visita:
+            modelo.addConstr(quicksum(partido_ijt[local,visita,fecha] for fecha in fechas) == 1)
 
 # R2
 for local in equipos:
-    modelo.addConstr(quicksum(partido_ijt[local,visita,fecha] for visita in equipos for fecha in fechas) == (len(equipos) - 1))
+    modelo.addConstr(quicksum(quicksum(partido_ijt[local,visita,fecha] for visita in equipos) for fecha in fechas) == (len(equipos) - 1))
 
 # R3
 for local in equipos:
@@ -78,7 +79,7 @@ for local in equipos:
 
 # R4
 for fecha in fechas:
-    modelo.addConstr(quicksum(partido_ijt[local,visita,fecha] for local in equipos for visita in equipos) == (len(equipos)//2))
+    modelo.addConstr(quicksum(quicksum(partido_ijt[local,visita,fecha] for local in equipos) for visita in equipos) == (len(equipos)//2))
 
 # R5
 for local in equipos:
@@ -87,14 +88,13 @@ for local in equipos:
 
 # R6
 for local in equipos:
-    modelo.addConstr(quicksum((partido_ijt[local,visita,fecha] + partido_ijt[visita,local,fecha]) for visita in equipos for fecha in range(1,(len(fechas)//2) + 1)) == (len(equipos) - 1))
+    modelo.addConstr(quicksum(quicksum((partido_ijt[local,visita,fecha] + partido_ijt[visita,local,fecha]) for visita in equipos) for fecha in range(1,(len(fechas)//2) + 1)) == (len(equipos) - 1))
 
 #R7
 for local in equipos:
     for visita in equipos:
-        modelo.addConstr(quicksum((partido_ijt[local, visita, fecha] + partido_ijt[visita, local, fecha]) for fecha in range(1,(len(fechas)//2) + 1)) == 1)
-
-
+        if local != visita:
+            modelo.addConstr(quicksum((partido_ijt[local, visita, fecha] + partido_ijt[visita, local, fecha]) for fecha in range(1,(len(fechas)//2) + 1)) == 1)
 
 # -----------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------RESTRICCIONES arbitraje-------------------------------------------------------------------
@@ -114,15 +114,17 @@ for local in equipos:
 for arb in arbitros:
     for local in equipos:
         for visita in equipos:
-            for fecha in fechas:
-                modelo.addConstr(arbitrar_aijt[arb,local,visita,fecha] <= partido_ijt[local,visita,fecha])
+            if local != visita:
+                for fecha in fechas:
+                    modelo.addConstr(arbitrar_aijt[arb,local,visita,fecha] <= partido_ijt[local,visita,fecha])
 
 # R11
 for arb in arbitros:
     for local in equipos:
         for visita in equipos:
-            for fecha in fechas:
-                modelo.addConstr(arbitrar_aijt[arb,local,visita,fecha] <= arbitrar_at[arb,fecha])
+            if local != visita:
+                for fecha in fechas:
+                    modelo.addConstr(arbitrar_aijt[arb,local,visita,fecha] <= arbitrar_at[arb,fecha])
 # -----------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------RESTRICCIONES consideraciones-------------------------------------------------------------------
 
@@ -130,29 +132,34 @@ for arb in arbitros:
 
 for local in equipos:
     for equipok in equipos:
-        for visita in equipos:
-            for fecha1 in range(2, (len(fechas)//2 + 1)):
-                for fecha2 in range((len(fechas)//2 + 2), (len(fechas) + 1)):
-                    modelo.addConstr((partido_ijt[local, visita, fecha1] + partido_ijt[visita, local, fecha1]
-                                 + partido_ijt[local, equipok, (fecha1 - 1)] + partido_ijt[equipok, local, (fecha1 - 1)]
-                                 + partido_ijt[local, visita, fecha2] + partido_ijt[visita, local, fecha2]
-                                 + partido_ijt[local, equipok, (fecha2 - 1)] + partido_ijt[equipok, local, (fecha2 - 1)]
-                                 - incumple_itn[local, fecha1, 1]) <= 3)
+        if equipok != local:
+            for visita in equipos:
+                if (visita != local) and (visita != equipok):
+                    for fecha1 in range(2, (len(fechas)//2 + 1)):
+                        for fecha2 in range((len(fechas)//2 + 2), (len(fechas) + 1)):
+                            modelo.addConstr((partido_ijt[local, visita, fecha1] + partido_ijt[visita, local, fecha1]
+                                              + partido_ijt[local, equipok, (fecha1 - 1)] + partido_ijt[equipok, local, (fecha1 - 1)]
+                                              + partido_ijt[local, visita, fecha2] + partido_ijt[visita, local, fecha2]
+                                              + partido_ijt[local, equipok, (fecha2 - 1)] + partido_ijt[equipok, local, (fecha2 - 1)]
+                                              - incumple_itn[local, fecha1, 1]) <= 3)
 
 # R13
 for local in equipos:
     for equipok in equipos:
-        for visita in equipos:
-            for fecha in range(2, (len(fechas) + 1)):
-                modelo.addConstr((partido_ijt[local, visita, (fecha - 1)] + partido_ijt[local, equipok, fecha] - 1) <= (incumple_itn[local, fecha, 2]))
+        if equipok != local:
+            for visita in equipos:
+                if (visita != local) and (visita != equipok):
+                    for fecha in range(2, (len(fechas) + 1)):
+                        modelo.addConstr((partido_ijt[local, visita, (fecha - 1)] + partido_ijt[local, equipok, fecha] - 1) <= (incumple_itn[local, fecha, 2]))
 
 # R14
 for local in equipos:
     for visita in equipos:
-        for arb in arbitros:
-            for fecha1 in range(1, (len(fechas)//2 + 1)):
-                for fecha2 in range((len(fechas)//2 + 1), len(fechas) + 1):
-                    modelo.addConstr((arbitrar_aijt[arb,local,visita,fecha1] + arbitrar_aijt[arb,visita,local,fecha2] - 1) <= incumple_itn[local,fecha1,3])
+        if local != visita:
+            for arb in arbitros:
+                for fecha1 in range(1, (len(fechas)//2 + 1)):
+                    for fecha2 in range((len(fechas)//2 + 1), len(fechas) + 1):
+                        modelo.addConstr((arbitrar_aijt[arb,local,visita,fecha1] + arbitrar_aijt[arb,visita,local,fecha2] - 1) <= incumple_itn[local,fecha1,3])
 
 # R15
 for local in equipos:
